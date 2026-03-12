5ef1c84beedcd95f826acbbcb0e97ecdf17beac7
enzocerneviva <contcerneviva@gmail.com>
2026-03-12 01:14:27 -0300

    refactor: ajustando arquivos que vão para o repositório

--- dist/interfaces/medico.d.ts
+++
@@ -1,9 +0,0 @@
-import type { Especialidade } from "../types/especialidade";
-export interface Medico {
-    id: number;
-    nome: string;
-    crm: string;
-    especialidade: Especialidade;
-    ativo: boolean;
-}
-//# sourceMappingURL=medico.d.ts.map
